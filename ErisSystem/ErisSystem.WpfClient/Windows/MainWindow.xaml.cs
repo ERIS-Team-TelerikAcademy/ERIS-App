a0440268493c5baa1234cfd1f5ef1bc23c7de034
Ivorankov <ivo_rankov@mail.bg>
2015-11-05 18:53:35 +0200

    WpfClient addition

---
+++ ErisSystem/ErisSystem.WpfClient/Windows/MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿namespace ErisSystem.WpfClient
+{
+    using Windows;
+    using System.Windows;
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnRegisterButtonClick(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void OnLoginButtonClick(object sender, RoutedEventArgs e)
+        {
+            var chatWindow = new ChatWindow();
+            chatWindow.Show();
+            this.Close();
+        }
+    }
+}
