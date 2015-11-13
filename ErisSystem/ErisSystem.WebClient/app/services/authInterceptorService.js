0e9bb297ddd525b259cc34a27cfb7cdccb993823
Toma Nikolov <on.tomas@gmail.com>
2015-11-13 23:02:20 +0200

    added interseptor for authenticated request

---
+++ ErisSystem/ErisSystem.WebClient/app/services/authInterceptorService.js
@@ -0,0 +1,28 @@
+'use strict';
+app.factory('authInterceptorService', ['$q', '$location', 'localStorageService',
+    function ($q, $location, localStorageService) {
+        var authInterceptorServiceFactory = {};
+
+        var request = function (config) {
+            config.headers = config.headers || {};
+
+            var authData = localStorageService.get('authorizationData');
+            if (authData) {
+                config.headers.Authorization = 'Bearer ' + authData.token;
+            }
+
+            return config;
+        };
+
+        var responseError = function (rejection) {
+            if (rejection.status === 401) {
+                $location.path('/login');
+            }
+            return $q.reject(rejection);
+        };
+
+        authInterceptorServiceFactory.request = request;
+        authInterceptorServiceFactory.responseError = responseError;
+
+        return authInterceptorServiceFactory;
+    }]);
