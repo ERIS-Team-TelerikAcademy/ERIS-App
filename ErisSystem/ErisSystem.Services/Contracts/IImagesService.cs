770cf46afd63ad910b1daac8e92b7f66dabb46d8
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-11-15 12:36:30 +0200

    Added images controller and service

---
+++ ErisSystem/ErisSystem.Services/Contracts/IImagesService.cs
@@ -0,0 +1,13 @@
+﻿namespace ErisSystem.Services.Contracts
+{
+    using ErisSystem.Models;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    public interface IImagesService
+    {
+        Task<int> Add(byte[] imageData, string extension, int forUserId);
+
+        IQueryable<Image> GetAll();
+    }
+}
