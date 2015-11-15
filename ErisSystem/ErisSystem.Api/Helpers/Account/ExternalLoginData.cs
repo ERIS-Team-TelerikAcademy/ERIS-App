b9502dcba1b7e987253c5e924330daf8b366716f
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-11-15 10:43:57 +0200

    Introduced Helpers folder and extracted account helpers there

---
+++ ErisSystem/ErisSystem.Api/Helpers/Account/ExternalLoginData.cs
@@ -0,0 +1,58 @@
+namespace ErisSystem.Api.Helpers.Account
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Security.Claims;
+
+    using Microsoft.AspNet.Identity;
+
+    public class ExternalLoginData
+    {
+        public string LoginProvider { get; set; }
+
+        public string ProviderKey { get; set; }
+
+        public string UserName { get; set; }
+
+        public IList<Claim> GetClaims()
+        {
+            IList<Claim> claims = new List<Claim>();
+            claims.Add(new Claim(ClaimTypes.NameIdentifier, ProviderKey, null, LoginProvider));
+
+            if (UserName != null)
+            {
+                claims.Add(new Claim(ClaimTypes.Name, UserName, null, LoginProvider));
+            }
+
+            return claims;
+        }
+
+        public static ExternalLoginData FromIdentity(ClaimsIdentity identity)
+        {
+            if (identity == null)
+            {
+                return null;
+            }
+
+            Claim providerKeyClaim = identity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (providerKeyClaim == null || String.IsNullOrEmpty(providerKeyClaim.Issuer)
+                || String.IsNullOrEmpty(providerKeyClaim.Value))
+            {
+                return null;
+            }
+
+            if (providerKeyClaim.Issuer == ClaimsIdentity.DefaultIssuer)
+            {
+                return null;
+            }
+
+            return new ExternalLoginData
+            {
+                LoginProvider = providerKeyClaim.Issuer,
+                ProviderKey = providerKeyClaim.Value,
+                UserName = identity.FindFirstValue(ClaimTypes.Name)
+            };
+        }
+    }
+}
