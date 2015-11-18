03ff7feda18cce2c603273e347520694f43f4217
Ivorankov <ivo_rankov@mail.bg>
2015-11-19 00:09:14 +0200

    added button click

--- ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
+++ ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
@@ -17,31 +17,11 @@ app.controller('contractController', ['$scope', 'contractData', function ($scope
 
                 }
             }
-        }).then(function(){
+        }).then(function () {
             $scope.data = dataCollection;
         });
+    $('body').on('click', '.userSelectionButton', function () {
+        alert('asd'); // And make post requests someday 
+    })
+
 }]);
-/*var contractItem = $('<li />');
-
-var deadline = $('<p/>');
-deadline.html('Deadline: ' + currentContract.Deadline);
-var name = $('<p/>');
-var userName = contractData
-    .getUserNickname(currentContract.ClientId)
-    .then(function (userData) {
-        name.html(userData.UserName);
-        contractItem.append(name);
-    });
-contractItem.append(deadline);
-
-var aprovalBtn = $('<button class="approvalButton userSelectionButton" />');
-var rejectionBtn = $('<button class="rejectButton userSelectionButton" />');
-
-contractItem.append(aprovalBtn);
-contractItem.append(rejectionBtn);
-
-if (currentContract.Status == 1) {
-    activeContractsContainer.append(contractItem);
-} else {
-    pendingContractsContainer.append(contractItem);
-}*/
