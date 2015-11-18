2df976b48ce4e96b6a6a6aae98410959aac5e88b
tomas <on.tomas@gmail.com>
2015-11-18 12:10:58 +0200

    added rating directive

---
+++ ErisSystem/ErisSystem.WebClient/app/hitmen-page/hitmen-data.js
@@ -0,0 +1,10 @@
+'use strict';
+app.factory('hitmenData', ['$http', '$q', 'data',
+    function ($http, $q, data) {
+        var baseUrl = '';
+        var hitmenData = {};
+
+
+
+        return hitmenData;
+    }]);
