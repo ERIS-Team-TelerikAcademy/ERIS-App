6b247e294ba0a9adc80cbf5b3224b39fd93f57e2
Ivorankov <ivo_rankov@mail.bg>
2015-11-18 23:44:16 +0200

    Modified view

--- ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
+++ ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
@@ -3,44 +3,45 @@ app.controller('contractController', ['$scope', 'contractData', function ($scope
 
     var activeContractsContainer = $('#activeContracts');
     var pendingContractsContainer = $('#pendingContracts');
+    var dataCollection = [];
     var contracts = contractData
         .getAll()
         .then(function (data) {
             console.log(data);
             for (var i = 0; i < data.length; i += 1) {
                 var currentContract = data[i];
-                var contractItem = $('<li />');
+                dataCollection[i] = {
+                    UserName: currentContract.ClientId,
+                    DeadLine: currentContract.Deadline,
+                    Status: currentContract.Status
 
-                var deadline = $('<p/>');
-                deadline.html('Deadline: ' + currentContract.Deadline);
-                var name = $('<p/>');
-                var userName = contractData
-                    .getUserNickname(currentContract.ClientId)
-                    .then(function(userData){
-                    name.html(userData.UserName);
-                        contractItem.append(name);
-                });
-
-                var aprovalBtn = $('<input type="button" class="btn btn-success userSelectionButton" />');
-                var rejectionBtn = $('<input type="button" class="btn btn-danger userSelectionButton" />');
-                contractItem.append(deadline);
-
-                contractItem.append(aprovalBtn);
-                contractItem.append(rejectionBtn);
-                aprovalBtn.text('+');
-                rejectionBtn.text('X');
-
-                if (currentContract.Status == 1) {
-                    activeContractsContainer.append(contractItem);
-                } else {
-                    pendingContractsContainer.append(contractItem);
                 }
             }
-        }).then(function () {
-            $('ul').on('Click', '.btn', function () {
-                alert('asdasd'); // Dun work :(
-                // Send put request to modify the contract(metadata in the html for contract recognition)
-                // Update page
-            })
+        }).then(function(){
+            $scope.data = dataCollection;
         });
 }]);
+/*var contractItem = $('<li />');
+
+var deadline = $('<p/>');
+deadline.html('Deadline: ' + currentContract.Deadline);
+var name = $('<p/>');
+var userName = contractData
+    .getUserNickname(currentContract.ClientId)
+    .then(function (userData) {
+        name.html(userData.UserName);
+        contractItem.append(name);
+    });
+contractItem.append(deadline);
+
+var aprovalBtn = $('<button class="approvalButton userSelectionButton" />');
+var rejectionBtn = $('<button class="rejectButton userSelectionButton" />');
+
+contractItem.append(aprovalBtn);
+contractItem.append(rejectionBtn);
+
+if (currentContract.Status == 1) {
+    activeContractsContainer.append(contractItem);
+} else {
+    pendingContractsContainer.append(contractItem);
+}*/
