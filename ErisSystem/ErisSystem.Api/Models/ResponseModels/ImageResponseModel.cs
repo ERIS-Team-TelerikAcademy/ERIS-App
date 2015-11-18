4f52655ef280760aaefb311b6605dd5612a3c728
Toma Nikolov <on.tomas@gmail.com>
2015-11-18 21:41:19 +0200

    fixed images

--- ErisSystem/ErisSystem.Api/Models/ResponseModels/ImageResponseModel.cs
+++ ErisSystem/ErisSystem.Api/Models/ResponseModels/ImageResponseModel.cs
@@ -7,5 +7,7 @@
         public string Extension { get; set; }
 
         public string UserId { get; set; }
+
+        public string Data { get; set; }
     }
 }
---
+++ ErisSystem/ErisSystem.Common/Helpers/Dropbox/DropBoxImageModel.cs
@@ -0,0 +1,11 @@
+﻿namespace ErisSystem.Common.Helpers.Dropbox
+{
+   public class DropBoxImageModel
+    {
+        public byte [] Data { get; set; }
+
+        public string ImageName { get; set; }
+
+        public string UserName { get; set; }
+    }
+}
