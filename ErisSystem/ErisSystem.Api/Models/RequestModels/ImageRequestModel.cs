b0707693e51196c2a9203006cef7763deb75638b
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-11-18 17:46:34 +0200

    Introduced image request model for the API

---
+++ ErisSystem/ErisSystem.Api/Models/RequestModels/ImageRequestModel.cs
@@ -0,0 +1,13 @@
+﻿namespace ErisSystem.Api.Models.RequestModels
+{
+    public class ImageRequestModel
+    {
+        public string Name { get; set; }
+
+        public string Extension { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Data { get; set; }
+    }
+}
