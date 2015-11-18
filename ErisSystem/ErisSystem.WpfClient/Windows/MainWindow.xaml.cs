8af9b1ae0939d7f6005aa8cfa5eb445af458fef8
Ivorankov <ivo_rankov@mail.bg>
2015-11-18 16:33:44 +0200

    Added user login validation

--- ErisSystem/ErisSystem.WpfClient/Windows/MainWindow.xaml.cs
+++ ErisSystem/ErisSystem.WpfClient/Windows/MainWindow.xaml.cs
@@ -2,6 +2,10 @@
 {
     using Windows;
     using System.Windows;
+    using System.Net;
+    using System.Text;
+    using System.IO;
+    using System;
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -21,11 +25,38 @@
 
         private void OnLoginButtonClick(object sender, RoutedEventArgs e)
         {
-            if (true)
+            var postData = "grant_type=password";
+            postData += "&username=" + this.UserName.Text;
+            postData += "&password=" + this.Password.Password;
+
+            var data = Encoding.ASCII.GetBytes(postData);
+            var request = (HttpWebRequest)WebRequest.Create("http://localhost:28499/Token");
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+
+            var responseString = string.Empty;
+            try
+            {
+                var response = (HttpWebResponse)request.GetResponse();
+                responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            if (responseString != "")
             {
                 var chatWindow = new ChatWindow(this.UserName.Text);
                 chatWindow.Show();
                 this.Close();
+                this.UserName.Text = "";
+                this.Password.Password = "";
             }
             else
             {
