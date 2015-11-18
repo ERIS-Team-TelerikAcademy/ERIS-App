82949be3bafb90e29b563fd971f958655e745355
Ivorankov <ivo_rankov@mail.bg>
2015-11-19 00:19:46 +0200

    Some small changes

--- ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
+++ ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
@@ -4,14 +4,24 @@ app.controller('contractController', ['$scope', 'contractData', function ($scope
     var activeContractsContainer = $('#activeContracts');
     var pendingContractsContainer = $('#pendingContracts');
     var dataCollection = [];
+    var userNames = [];
     var contracts = contractData
-        .getAll()
+        .getAll().then(function (data) {
+            for (var i = 0; i < data.length; i += 1){
+                var currentContract = data[i];
+                contractData.getUserNickname(currentContract.ClientId).then(function(userData){
+                    userNames[userData.ClientId] = userData.UserName;
+                });
+                //this is fucked
+            }
+            return data;
+        })
         .then(function (data) {
             console.log(data);
             for (var i = 0; i < data.length; i += 1) {
                 var currentContract = data[i];
                 dataCollection[i] = {
-                    UserName: currentContract.ClientId,
+                    UserName: userNames[currentContract.ClientId],
                     DeadLine: currentContract.Deadline,
                     Status: currentContract.Status
 
@@ -21,7 +31,7 @@ app.controller('contractController', ['$scope', 'contractData', function ($scope
             $scope.data = dataCollection;
         });
     $('body').on('click', '.userSelectionButton', function () {
-        alert('asd'); // And make post requests someday 
+        alert('asd'); // And make post requests someday
     })
 
 }]);
