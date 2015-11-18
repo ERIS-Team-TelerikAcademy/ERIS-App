3500b78dd89000541599f9511379622b15b740fc
TemplarRei <boris.stoyanovv@gmail.com>
2015-11-18 14:42:41 +0200

    Beautifying.

--- ErisSystem/ErisSystem.Api/Controllers/UserController.cs
+++ ErisSystem/ErisSystem.Api/Controllers/UserController.cs
@@ -24,27 +24,7 @@
         {
             this.hitmen = hitmenServices;
         }
-
-        ///// <summary>
-        ///// Gets a user by id
-        ///// </summary>
-        ///// <param name="id">the id to search for in the db</param>
-        ///// <returns>the response model of the queried user</returns>
-        //[Route("{id}")]
-        //[HttpGet]
-        //public IHttpActionResult GetHitmanById(int id)
-        //{
-        //    var result = Mapper.Map<UserResponseModel>(this.hitmen
-        //        .GetById(id));
-            
-        //    if (result == null)
-        //    {
-        //        return this.NotFound();
-        //    }
-
-        //    return this.Ok(result);
-        //}
-
+        
         [Route("{userName}")]
         [HttpGet]
         public IHttpActionResult GetHitmanByUserName(string userName)
@@ -74,30 +54,7 @@
 
             return this.Ok(result);
         }
-
-        /// <summary>
-        /// Adds a new hitman to the DB
-        /// </summary>
-        /// <param name="model">A hitman response model from the body of the query</param>
-        /// <returns>the ID of the added hitman</returns>
-        //[Route("register")]
-        //[HttpPost]
-        //public IHttpActionResult Post([FromBody]UserResponseModel model)
-        //{
-        //    if (!this.ModelState.IsValid)
-        //    {
-        //        return this.BadRequest(this.ModelState);
-        //    }
-
-        //    var newHitmanId = this.hitmen.Add(
-        //        model.Nickname,
-        //        model.AboutMe,
-        //        model.Gender,
-        //        model.Password);
-
-        //    return this.Created(this.Url.ToString(), newHitmanId);
-        //}
-
+               
         /// <summary>
         /// Not sure what to do here.
         /// </summary>
--- ErisSystem/ErisSystem.Api/Models/ResponseModels/ContractResponseModel.cs
+++ ErisSystem/ErisSystem.Api/Models/ResponseModels/ContractResponseModel.cs
@@ -18,20 +18,5 @@
         public int HitStatus { get; set; }
 
         public DateTime Deadline { get; set; }
-
-        //public void CreateMappings(IConfiguration configuration)
-        //{
-        //    //configuration.CreateMap<ContractResponseModel, Contract>()
-        //    //    .ForMember(c => c.HitmanId, opts => opts.MapFrom(c => c.HitmanId))
-        //    //    .ForMember(c => c.ClientId, opts => opts.MapFrom(c => c.ClientId))
-        //    //    .ForMember(c => c.Status, opts => opts.MapFrom(c => (ConnectionStatus)c.Status))
-        //    //    .ForMember(c => c.HitStatus, opts => opts.MapFrom(c => (HitStatus)c.HitStatus));
-
-        //    configuration.CreateMap<Contract, ContractResponseModel>()
-        //        .ForMember(c => c.HitmanId, opts => opts.MapFrom(c => c.HitmanId))
-        //        .ForMember(c => c.ClientId, opts => opts.MapFrom(c => c.ClientId))
-        //        .ForMember(c => c.Status, opts => opts.MapFrom(c => (int)c.Status))
-        //        .ForMember(c => c.HitStatus, opts => opts.MapFrom(c => (int)c.HitStatus));
-        //}
     }
 }
