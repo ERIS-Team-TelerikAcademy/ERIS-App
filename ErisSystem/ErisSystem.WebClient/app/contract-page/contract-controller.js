24634b8d3c03e749841a81cb873231b7ce0e46ef
Ivorankov <ivo_rankov@mail.bg>
2015-11-19 13:56:13 +0200

    Fixed user name parsing

--- ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
+++ ErisSystem/ErisSystem.WebClient/app/contract-page/contract-controller.js
@@ -7,29 +7,33 @@ app.controller('contractController', ['$scope', 'contractData', function ($scope
     var userNames = [];
     var contracts = contractData
         .getAll().then(function (data) {
-            for (var i = 0; i < data.length; i += 1){
+            var i = 0;
+            var j = 0;
+            for (i = 0; i < data.length; i += 1){
                 var currentContract = data[i];
-                contractData.getUserNickname(currentContract.ClientId).then(function(userData){
-                    userNames[userData.ClientId] = userData.UserName;
-                });
-                //this is fucked
-            }
-            return data;
-        })
-        .then(function (data) {
-            console.log(data);
-            for (var i = 0; i < data.length; i += 1) {
-                var currentContract = data[i];
-                dataCollection[i] = {
-                    UserName: userNames[currentContract.ClientId],
-                    DeadLine: currentContract.Deadline,
-                    Status: currentContract.Status
+                contractData.getUserNickname(currentContract.ClientId)
+                    .then(function(userData){
+                    userNames[j] = userData.UserName;
+                        j++;
+                    return data;
 
-                }
+                }).then(function (data) {
+                    console.log(data);
+                    for (var i = 0; i < data.length; i += 1) {
+                        var currentContract = data[i];
+                        dataCollection[i] = {
+                            UserName: userNames[i],
+                            DeadLine: currentContract.Deadline,
+                            Status: currentContract.Status
+                        }
+                    }
+                })
             }
-        }).then(function () {
-            $scope.data = dataCollection;
+
         });
+            $scope.data = dataCollection;
+
+
     $('body').on('click', '.userSelectionButton', function () {
         alert('asd'); // And make post requests someday
     })
