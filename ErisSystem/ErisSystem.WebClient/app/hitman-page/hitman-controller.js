c21959d2092d1922800a79aea7e53b891557194e
Toma Nikolov <on.tomas@gmail.com>
2015-11-19 23:33:46 +0200

    little changes in profile view

--- ErisSystem/ErisSystem.WebClient/app/hitman-page/hitman-controller.js
+++ ErisSystem/ErisSystem.WebClient/app/hitman-page/hitman-controller.js
@@ -1,13 +1,14 @@
 'use strict';
-app.controller('hitmanController', ['$scope', '$routeParams', '$rootScope', 'hitmanData',
-    function ($scope, $routeParams, $rootScope, hitmanData) {
-        var userName = $routeParams.name;
+app.controller('hitmanController', ['$scope', '$routeParams', 'authData', 'hitmanData',
+    function ($scope, $routeParams, authData, hitmanData) {
+        $scope.userName = $routeParams.name;
 
-        hitmanData.getByUserName(userName)
+        hitmanData.getByUserName($scope.userName)
             .then(function (response) {
                 $scope.hitman = response;
-            })
+            });
 
+        $scope.authentication = authData.authentication;
         $scope.rating = 2;
         $scope.rateFunction = function (rating) {
 
